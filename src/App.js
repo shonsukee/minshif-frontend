d33df79ab9aadde950179ff8617323745825698a
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-01-25 15:01:45 +0900

    [Add]Googleログインページ追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./components/GoogleResource/GoogleLogin";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
